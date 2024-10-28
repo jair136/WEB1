cb08685e4b528cb9e6de2c607a4578deb53f4f74
Jair <jair136@hotmail.com>
2024-10-28 00:05:41 -0500

    Cambios 2

--- api.php
+++ api.php
@@ -1,10 +1,22 @@
 <?php
-    include('conectdb.php');
-    $id=$_GET["id"];
-    if($result=obtenerResultado("SELECT * FROM  `anaimal` where id = '$id'")){
-        while($fila=$result->fetch_array(mode: MYSQLI_NUM))
-        {
-            echo json_encode(value: $fila);
+include('conectdb.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST["id"])) {
+    $id = $_POST["id"];
+    
+    if ($result = obtenerResultado("SELECT * FROM `animal` WHERE id = '$id'")) {
+        while ($fila = $result->fetch_array(MYSQLI_NUM)) {
+            echo json_encode($fila);
         }
     }
-?>    
+} else {
+    
+    echo '
+    <form method="POST" action="">
+        <label for="id">Ingrese el ID:</label>
+        <input type="text" name="id" id="id" required>
+        <button type="submit">Consultar</button>
+    </form>
+    ';
+}
+?>
--- conectdb.php
+++ conectdb.php
@@ -1,13 +1,17 @@
 <?php
-header(header: 'content-type: text/html; charset=utf-8');
-function obtenerResultado($qwery){
-    $mysqli=new mysqli("localhost" "root" "" "web ejercicios");
-    if($mysqli->conect_errno){
-       printf("No me puedo conectar");
+header('Content-Type: text/html; charset=utf-8');
+
+function obtenerResultado($query) {
+    $mysqli = new mysqli("localhost", "root", "", "web_ejercicios");
+    if ($mysqli->connect_errno) {
+        printf("No me puedo conectar");
+        return null;
     }
-    if($mysqli->multi_query($qwery)){
+    if ($mysqli->multi_query($query)) {
         return $mysqli->store_result();
     }
+
     $mysqli->close();
-}    
+    return null;
+}
 ?>
